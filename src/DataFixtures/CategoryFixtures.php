d301f2f068dd49ed02c3fb4ba9844367fcbe50f1
StanaA <sfxayanne@hotmail.com>
2023-12-21 18:33:04 +0100

    Cours Symfony

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public const CATEGORY_TOYS = 'CATEGORY_TOYS';
+    public function load(ObjectManager $manager): void
+    {
+         $travels = new Category();
+         $travels->setName('Voyages');
+         $manager->persist($travels);
+
+        
+        $donkeys = new Category();
+        $donkeys->setName('Avec des anes modifer');
+        $donkeys->SetParent($travels);  
+        $manager->persist($donkeys);
+
+
+
+        $toys = new Category();
+        $toys->setName('Jouets');
+        $manager->persist($toys);
+
+        $this->addReference(self::CATEGORY_TOYS, $toys);
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProductFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $product = new Product();
+        $product->setTitle('Ane en peluche');
+        $product->setCategory($this->getReference(CategoryFixtures::CATEGORY_TOYS));
+        $product->setPrice('1000');
+        $product->setDescription('blablablabla');
+        $manager->persist($product);
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return  [
+            CategoryFixtures::class
+        ];
+    }
+
+}
